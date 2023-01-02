78b616474b5c6720a0ff164df3ecda3da7e30b74
noga lavan <noga0802@gmail.com>
2023-01-02 09:15:46 +0200

    wip

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -38,19 +38,19 @@ router.get('/stockItem/getAll', async (req, res) => {
     }
 })
 
-//Get by ID Method
-router.get('/stockItem/getOne/:id', async (req, res) => {
-    try{
-        const data = await StockItem.findById(req.params.id);
-        res.json(data)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
+// //Get by ID Method
+// router.get('/stockItem/getOne/:id', async (req, res) => {
+//     try{
+//         const data = await StockItem.findById(req.params.id);
+//         res.json(data)
+//     }
+//     catch(error){
+//         res.status(500).json({message: error.message})
+//     }
+// })
 
 //Update by ID Method
-router.patch('/stockItem/update/:id', async (req, res) => {
+router.put('/stockItem/update/:id', async (req, res) => {
     try {
         const id = req.params.id;
         const updatedData = req.body;
@@ -67,16 +67,16 @@ router.patch('/stockItem/update/:id', async (req, res) => {
     }
 })
 
-//Delete by ID Method
-router.delete('/stockItem/delete/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const data = await StockItem.findByIdAndDelete(id)
-        res.send(`Document with ${data.name} has been deleted..`)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-})
+// //Delete by ID Method
+// router.delete('/stockItem/delete/:id', async (req, res) => {
+//     try {
+//         const id = req.params.id;
+//         const data = await StockItem.findByIdAndDelete(id)
+//         res.send(`Document with ${data.name} has been deleted..`)
+//     }
+//     catch (error) {
+//         res.status(400).json({ message: error.message })
+//     }
+// })
 
 module.exports = router;
